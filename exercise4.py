3856e2dcaf30ab398ce72aaab46e50804dd17031
Arundhati1Sharma <49611954+Arundhati1Sharma@users.noreply.github.com>
2020-05-13 11:45:49 +0530

    Create exercise4.py

---
+++ exercise4.py
@@ -0,0 +1,76 @@
+import tensorflow as tf
+import os
+import zipfile
+from os import path, getcwd, chdir
+
+# DO NOT CHANGE THE LINE BELOW. If you are developing in a local
+# environment, then grab happy-or-sad.zip from the Coursera Jupyter Notebook
+# and place it inside a local folder and edit the path to that location
+path = f"{getcwd()}/../tmp2/happy-or-sad.zip"
+
+zip_ref = zipfile.ZipFile(path, 'r')
+zip_ref.extractall("/tmp/h-or-s")
+zip_ref.close()
+# GRADED FUNCTION: train_happy_sad_model
+def train_happy_sad_model():
+    # Please write your code only where you are indicated.
+    # please do not remove # model fitting inline comments.
+
+    DESIRED_ACCURACY = 0.999
+
+    class myCallback(tf.keras.callbacks.Callback):
+        def on_epoch_end(self, epoch, logs={}):
+            if(logs.get('acc')>DESIRED_ACCURACY):
+                print("\nReached 99.9% accuracy so cancelling training!")
+                self.model.stop_training = True
+
+    callbacks = myCallback()
+    
+    # This Code Block should Define and Compile the Model. Please assume the images are 150 X 150 in your implementation.
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(16,(3,3), activation='relu', input_shape=(150, 150, 3), padding = "same"),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32,(3,3), activation='relu', padding = "same"),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64,(3,3), activation='relu', padding = "same"),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation = tf.nn.relu),
+    tf.keras.layers.Dense(1, activation = tf.nn.sigmoid)
+    ])
+
+    from tensorflow.keras.optimizers import RMSprop
+    adam = tf.keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+
+    model.compile(optimizer = adam, loss='binary_crossentropy', metrics = ['acc'])
+        
+
+    # This code block should create an instance of an ImageDataGenerator called train_datagen 
+    # And a train_generator by calling train_datagen.flow_from_directory
+
+    from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+    train_datagen = ImageDataGenerator(rescale=1/255)
+
+    # Please use a target_size of 150 X 150.
+    train_generator = train_datagen.flow_from_directory(
+        '/tmp/h-or-s/',  
+        target_size=(150, 150),
+        batch_size=1,
+        class_mode='binary')
+    # Expected output: 'Found 80 images belonging to 2 classes'
+
+    # This code block should call model.fit_generator and train for
+    # a number of epochs.
+    # model fitting
+    history = model.fit_generator(
+          train_generator,
+      steps_per_epoch=5,  
+      epochs=15,
+      verbose=1,
+      callbacks=[callbacks])
+    # model fitting
+    return history.history['acc'][-1]
+    
+# The Expected output: "Reached 99.9% accuracy so cancelling training!""
+train_happy_sad_model()    
